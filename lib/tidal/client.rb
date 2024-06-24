67f7db31d56c4890f92d4459d9a07f61ca61d4ca
Fabrice Renard <fabrice.renard12@outlook.com>
2024-06-23 20:08:31 -0400

    Add client class to Tidal module

--- lib/tidal/client.rb
+++ lib/tidal/client.rb
@@ -1,16 +1,18 @@
 require 'oauth2'
 
-class Client
-  BASE_URL = 'https://api.tidal.com/v1'.freeze
-  TOKEN_URL = 'https://auth.tidal.com/v1/oauth2/token'.freeze
+module Tidal
+  class Client
+    BASE_URL = 'https://api.tidal.com/v1'.freeze
+    TOKEN_URL = 'https://auth.tidal.com/v1/oauth2/token'.freeze
 
-  def initialize(client_id, client_secret)
-    @client_id = client_id
-    @client_secret = client_secret
-    @client = OAuth2::Client.new(@client_id, @client_secret, site: BASE_URL, token_url: TOKEN_URL)
-  end
+    def initialize(client_id, client_secret)
+      @client_id = client_id
+      @client_secret = client_secret
+      @client = OAuth2::Client.new(@client_id, @client_secret, site: BASE_URL, token_url: TOKEN_URL)
+    end
 
-  def get_access_token
-    @client.client_credentials.get_token.token
+    def get_access_token
+      @client.client_credentials.get_token.token
+    end
   end
 end

dd9726426be265ba16a9eeb1e573e90aee9fa68a
Fabrice Renard <fabrice.renard12@outlook.com>
2024-06-23 20:45:33 -0400

    Add first api call

--- example.rb
+++ example.rb
@@ -5,9 +5,7 @@ client_id = 'xBH98O1UTiWRn5np'
 client_secret = 'e5hjNXZEJ294afD7N1ArM5y1tv7jogK0fxbNR0jWlgY='
 
 # Create an instance of TidalOAuthClient
-oauth_client = Tidal::Client.new(client_id, client_secret)
+client = Tidal::Client.new(client_id, client_secret)
 
-# Obtain the access token
-access_token = oauth_client.get_access_token
-
-puts "Access Token: #{access_token}"
+puts "Access Token: #{client.access_token}"
+puts client.get_single_album(251380836, "US")
--- lib/tidal/client.rb
+++ lib/tidal/client.rb
@@ -2,17 +2,31 @@ require 'oauth2'
 
 module Tidal
   class Client
-    BASE_URL = 'https://api.tidal.com/v1'.freeze
+    BASE_URL = 'https://openapi.tidal.com/'.freeze
     TOKEN_URL = 'https://auth.tidal.com/v1/oauth2/token'.freeze
+
+    attr_reader :access_token
 
     def initialize(client_id, client_secret)
       @client_id = client_id
       @client_secret = client_secret
       @client = OAuth2::Client.new(@client_id, @client_secret, site: BASE_URL, token_url: TOKEN_URL)
+      @access_token = @client.client_credentials.get_token.token
     end
 
-    def get_access_token
-      @client.client_credentials.get_token.token
+    def get_single_album(album_id, country_code)
+      response = @client.request(:get, "#{BASE_URL}/albums/#{album_id}?countryCode=#{country_code}", headers: default_headers)
+      JSON.parse(response.body)
+    end
+
+    private
+
+    def default_headers
+      {
+        'accept' => "application/vnd.tidal.v1+json",
+        'Authorization' => "Bearer #{@access_token}",
+        'Content-Type' => "application/vnd.tidal.v1+json",
+      }
     end
   end
 end
